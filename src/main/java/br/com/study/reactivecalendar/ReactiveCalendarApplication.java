c534820d7d0bf2f352811b81bf58a497c8e5697b
José Luiz Abreu Cardoso Junior <junior@MacBook-Pro-de-Jose.local>
2022-03-07 21:54:36 -0300

    criação da estrutura inicial do projeto

---
+++ src/main/java/br/com/study/reactivecalendar/ReactiveCalendarApplication.java
@@ -0,0 +1,13 @@
+package br.com.study.reactivecalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/study/reactivecalendar/ReactiveCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.study.reactivecalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
