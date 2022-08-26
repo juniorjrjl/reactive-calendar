00550c2bf7a7b326f57cf48f364d3e90d5e2d1f3
Junior <junior.jr.jl@gmail.com>
2022-08-26 14:03:42 -0300

    testando a criação de compromissos

---
+++ src/test/java/br/com/study/reactivecalendar/core/factoryBot/dto/AppointmentDTOFactoryBot.java
@@ -0,0 +1,75 @@
+package br.com.study.reactivecalendar.core.factoryBot.dto;
+
+import br.com.study.reactivecalendar.domain.document.GuestType;
+import br.com.study.reactivecalendar.domain.dto.AppointmentDTO;
+import br.com.study.reactivecalendar.domain.dto.GuestDTO;
+import com.github.javafaker.Faker;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+
+import java.time.OffsetDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static br.com.study.reactivecalendar.core.RandomData.between;
+import static br.com.study.reactivecalendar.core.RandomData.birthday;
+import static br.com.study.reactivecalendar.core.RandomData.getFaker;
+import static br.com.study.reactivecalendar.core.RandomData.randomEnum;
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class AppointmentDTOFactoryBot {
+
+    public static AppointmentDTOFactoryBotBuilder builder(){
+        return new AppointmentDTOFactoryBotBuilder();
+    }
+
+    public static class AppointmentDTOFactoryBotBuilder{
+
+        private final String id;
+        private final String title;
+        private final String details;
+        private final OffsetDateTime startIn;
+        private final OffsetDateTime endIn;
+        private final Set<GuestDTO> guests;
+        private final OffsetDateTime createdAt;
+        private final OffsetDateTime updatedAt;
+        private final Faker faker = getFaker();
+
+        public AppointmentDTOFactoryBotBuilder() {
+            this.id = ObjectId.get().toString();
+            this.title = faker.lorem().sentence(100);
+            this.details = faker.lorem().sentence(255);
+            this.startIn = birthday();
+            this.endIn = between(startIn, OffsetDateTime.now().plusYears(80L));
+            this.guests = generateGuests(faker.number().randomDigitNotZero());
+            this.createdAt = OffsetDateTime.now();
+            this.updatedAt = OffsetDateTime.now();
+        }
+
+        private Set<GuestDTO> generateGuests(final int amount){
+            return Stream.generate(() ->  GuestDTO.builder().userId(ObjectId.get().toString())
+                            .type(randomEnum(GuestType.class))
+                            .email(faker.internet().emailAddress())
+                            .build())
+                    .limit(amount)
+                    .collect(Collectors.toSet());
+        }
+
+        public AppointmentDTO build(){
+            return AppointmentDTO.builder()
+                    .id(id)
+                    .title(title)
+                    .details(details)
+                    .startIn(startIn)
+                    .endIn(endIn)
+                    .guests(guests)
+                    .createdAt(createdAt)
+                    .updatedAt(updatedAt)
+                    .build();
+        }
+
+    }
+
+}
--- src/test/java/br/com/study/reactivecalendar/domain/service/AppointmentServiceTest.java
+++ src/test/java/br/com/study/reactivecalendar/domain/service/AppointmentServiceTest.java
@@ -2,9 +2,11 @@ package br.com.study.reactivecalendar.domain.service;
 
 import br.com.study.reactivecalendar.core.factoryBot.document.AppointmentDocumentFactoryBot;
 import br.com.study.reactivecalendar.core.factoryBot.document.UserDocumentFactoryBot;
+import br.com.study.reactivecalendar.core.factoryBot.dto.AppointmentDTOFactoryBot;
 import br.com.study.reactivecalendar.domain.document.AppointmentDocument;
 import br.com.study.reactivecalendar.domain.document.UserDocument;
 import br.com.study.reactivecalendar.domain.dto.MailMessageDTO;
+import br.com.study.reactivecalendar.domain.exception.ConflictException;
 import br.com.study.reactivecalendar.domain.exception.NotFoundException;
 import br.com.study.reactivecalendar.domain.mapper.AppointmentMapper;
 import br.com.study.reactivecalendar.domain.mapper.AppointmentMapperImpl;
@@ -16,6 +18,7 @@ import br.com.study.reactivecalendar.domain.mapper.MailMapperImpl_;
 import br.com.study.reactivecalendar.domain.repository.AppointmentRepository;
 import br.com.study.reactivecalendar.domain.service.query.AppointmentQueryService;
 import br.com.study.reactivecalendar.domain.service.query.UserQueryService;
+import com.github.javafaker.Faker;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,14 +31,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
+import static br.com.study.reactivecalendar.core.RandomData.getFaker;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -67,10 +74,78 @@ public class AppointmentServiceTest {
 
     private AppointmentService appointmentService;
 
+    private final Faker faker = getFaker();
+
     @BeforeEach
     void setup(){
         appointmentService = new AppointmentService(appointmentRepository, appointmentQueryService, userQueryService,
                 mailService, guestMapper, mailMapper, appointmentMapper);
+    }
+
+    @Test
+    void saveTest() throws InterruptedException {
+        var appointment = AppointmentDTOFactoryBot.builder().build();
+        var users = Stream.generate(() -> UserDocumentFactoryBot.builder().build())
+                .limit(appointment.guests().size())
+                .toList();
+        var usersEmailReplaced = new ArrayList<UserDocument>();
+        for (int i = 0; i < users.size(); i++) {
+            var guests = new ArrayList<>(appointment.guests());
+            usersEmailReplaced.add(users.get(i).toBuilder().email(guests.get(i).email()).build());
+        }
+        when(userQueryService.findByEmail(anyString())).thenAnswer(invocation -> {
+            var email = invocation.getArgument(0, String.class);
+            return Mono.defer(() -> Mono.just(usersEmailReplaced.stream().filter(u -> u.email().equals(email)).findFirst().orElseThrow()));
+        });
+        when(appointmentQueryService.findAppointmentsWithUserInInterval(anyString(), any(OffsetDateTime.class), any(OffsetDateTime.class)))
+                .thenReturn(Flux.empty());
+        when(appointmentRepository.save(any(AppointmentDocument.class)))
+                .thenAnswer(invocation -> {
+                    var document = invocation.getArgument(0, AppointmentDocument.class);
+                    document = document.toBuilder()
+                            .id(ObjectId.get().toString())
+                            .createdAt(OffsetDateTime.now())
+                            .updatedAt(OffsetDateTime.now())
+                            .build();
+                    return Mono.just(document);
+                });
+        when(mailService.send(mailMessageCaptor.capture())).thenReturn(Mono.empty());
+        StepVerifier.create(appointmentService.save(appointment))
+                .assertNext(actual ->{
+                    assertThat(actual).isNotNull();
+                })
+                        .verifyComplete();
+        TimeUnit.SECONDS.sleep(4L);
+
+        var mailMessage = mailMessageCaptor.getValue();
+        assertThat(((OffsetDateTime)mailMessage.variables().get("startIn")).toEpochSecond()).isEqualTo(appointment.startIn().toEpochSecond());
+        assertThat(((OffsetDateTime)mailMessage.variables().get("endIn")).toEpochSecond()).isEqualTo(appointment.endIn().toEpochSecond());
+        assertThat(((String)mailMessage.variables().get("title"))).isEqualTo(appointment.title());
+        assertThat(((String)mailMessage.variables().get("details"))).isEqualTo(appointment.details());
+        assertThat(mailMessage.destinations()).containsAnyElementsOf(usersEmailReplaced.stream().map(UserDocument::email).toList());
+    }
+
+    @Test
+    void whenSomeUserHasAnotherAppointmentInSameIntervalThenThrowException(){
+        var appointment = AppointmentDTOFactoryBot.builder().build();
+        var users = Stream.generate(() -> UserDocumentFactoryBot.builder().build())
+                .limit(appointment.guests().size())
+                .toList();
+        var userCount = new AtomicInteger(0);
+        var userAppointmentCount = new AtomicInteger(0);
+        var userInteractionToGenerateError = faker.number().numberBetween(0, userCount.get());
+        when(userQueryService.findByEmail(anyString())).thenReturn(Mono.defer(() -> Mono.just(users.get(userCount.getAndIncrement()))));
+        when(appointmentQueryService.findAppointmentsWithUserInInterval(anyString(), any(OffsetDateTime.class), any(OffsetDateTime.class)))
+                .thenReturn(Flux.defer(() -> userInteractionToGenerateError == userAppointmentCount.getAndIncrement() ?
+                        Flux.fromIterable(List.of(AppointmentDocumentFactoryBot.builder().build())) :
+                        Flux.empty()));
+        StepVerifier.create(appointmentService.save(appointment))
+                .verifyError(ConflictException.class);
+        verify(userQueryService, times(userCount.get())).findByEmail(anyString());
+        verify(appointmentQueryService, times(userAppointmentCount.get()))
+                .findAppointmentsWithUserInInterval(anyString(), any(OffsetDateTime.class), any(OffsetDateTime.class));
+        verify(appointmentRepository, times(0)).save(any(AppointmentDocument.class));
+        verify(mailService, times(0)).send(any(MailMessageDTO.class));
     }
 
     @Test
